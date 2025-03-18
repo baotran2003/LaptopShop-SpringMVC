9d613df5b8e84d2952038101fc8588e76fc140f9
Bao Tran <trankhuongbao2003@gmail.com>
2025-03-19 00:11:57 +0700

    updateUser

--- src/main/java/vn/baotran/laptopshop/controller/UserController.java
+++ src/main/java/vn/baotran/laptopshop/controller/UserController.java
@@ -2,10 +2,7 @@ package vn.baotran.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import vn.baotran.laptopshop.domain.User;
 import vn.baotran.laptopshop.service.UserService;
 
@@ -53,6 +50,26 @@ public class UserController {
     public String createUserPage(@ModelAttribute User newUser) {
         System.out.println("Run here !" + newUser);
         this.userService.handleSaveUser(newUser);
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping("/admin/user/update/{id}")
+    public String getUpdateUserPage(Model model, @PathVariable Long id) {
+        User currentUser = this.userService.getUserById(id);
+        model.addAttribute("newUser", currentUser);
+        return "admin/user/update";
+    }
+
+    @PostMapping("/admin/user/update")
+    public String postUpdateUser(@ModelAttribute("newUser") User user) {
+        User currentUser = this.userService.getUserById(user.getId());
+        if(currentUser != null) {
+            currentUser.setAddress(user.getAddress());
+            currentUser.setFullName(user.getFullName());
+            currentUser.setPhone(user.getPhone());
+
+            this.userService.handleSaveUser(user);
+        }
         return "redirect:/admin/user";
     }
 }
